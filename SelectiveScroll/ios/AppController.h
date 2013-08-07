86927e5a81dc6c3b094650b5f1fb6c4ba2cd347f
TOSHITAKA NAKAGAWA <tnakagawa@Nakagawa-no-MacBook-Pro.local>
2013-08-07 09:05:40 +0900

    Initial Commit

---
+++ SelectiveScroll/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  SelectiveScrollAppController.h
+//  SelectiveScroll
+//
+//  Created by TOSHITAKA NAKAGAWA on 2013/08/07.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ SelectiveScroll/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SelectiveScrollAppController.h
+//  SelectiveScroll
+//
+//  Created by TOSHITAKA NAKAGAWA on 2013/08/07.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
